ce51b6ad871c99e44bdb4eaa8b685c0f5cf3dcd7
Noah S <noahs9576@gmail.com>
2023-04-03 19:46:48 -0400

    First update editor

--- editor.js
+++ editor.js
@@ -1,39 +1,94 @@
-function SetStyle(selectObj){
-    $("textarea").css("font-family", selectObj.value);
+// function SetStyle(selectObj) {
+//   $("text-wrapper")
+//     .is('[contenteditable="true"]')
+//     .css("font-family", selectObj.value);
+// }
+
+function SetStyle(selectObj) {
+  $('.text-wrapper > [contenteditable="true"]').css(
+    "font-family",
+    selectObj.value
+  );
 }
 
-function SetColor(selectObj){
-    $("textarea").css("color", selectObj.value);
+function SetColor(selectObj) {
+  $('.text-wrapper > [contenteditable="true"]').css("color", selectObj.value);
 }
 
-function SetSize(selectObj){
-    $("textarea").css("font-size", selectObj.value + "px");
+function SetSize(selectObj) {
+  $('.text-wrapper > [contenteditable="true"]').css(
+    "font-size",
+    selectObj.value + "px"
+  );
 }
 
-function ToggleBold(){
-    $("textarea").css("font-weight", $("textarea").css("font-weight") == "bold" ? "normal" : "bold"); 
+function ToggleBold() {
+  $('.text-wrapper > [contenteditable="true"]').css(
+    "font-weight",
+    $('.text-wrapper > [contenteditable="true"]').css("font-weight") == "bold"
+      ? "normal"
+      : "bold"
+  );
 }
 
-function ToggleItalic(){
-    $("textarea").css("font-style", $("textarea").css("font-style") == "italic" ? "normal" : "italic");
+function ToggleItalic() {
+  $('.text-wrapper > [contenteditable="true"]').css(
+    "font-style",
+    $('.text-wrapper > [contenteditable="true"]').css("font-style") == "italic"
+      ? "normal"
+      : "italic"
+  );
 }
 
-function ToggleUnderline(){
-    $("textarea").css("text-decoration", $("textarea").css("text-decoration") == "underline" ? "none" : "underline");
+function ToggleUnderline() {
+  $('.text-wrapper > [contenteditable="true"]').css(
+    "text-decoration",
+    $('.text-wrapper > [contenteditable="true"]').css("text-decoration") ==
+      "underline"
+      ? "none"
+      : "underline"
+  );
 }
 
-function ToggleStrike(){
-    $("textarea").css("text-decoration", $("textarea").css("text-decoration") == "line-through" ? "none" : "line-through");
+function ToggleStrike() {
+  $('.text-wrapper > [contenteditable="true"]').css(
+    "text-decoration",
+    $('.text-wrapper > [contenteditable="true"]').css("text-decoration") ==
+      "line-through"
+      ? "none"
+      : "line-through"
+  );
 }
 
-function SetLeft(){
-    $("textarea").css("text-align", "left");
+function SetLeft() {
+  $('.text-wrapper > [contenteditable="true"]').css("text-align", "left");
 }
 
-function SetCenter(){    
-    $("textarea").css("text-align", "center");
+function SetCenter() {
+  $('.text-wrapper > [contenteditable="true"]').css("text-align", "center");
 }
 
-function SetRight(){
-    $("textarea").css("text-align", "right");
+function SetRight() {
+  $('.text-wrapper > [contenteditable="true"]').css("text-align", "right");
+}
+
+function ToggleEdit(editButton) {
+  $(".text-wrapper > *").attr(
+    "data-editable",
+    $(".text-wrapper > *").attr("data-editable") == "true" ? "false" : "true"
+  );
+
+  editButton.innerHTML =
+    $(".text-wrapper > *").attr("data-editable") == "true" ? "Save" : "Edit";
+
+  $(".text-wrapper > *").attr("contenteditable", false);
+}
+
+function StartEdit(element) {
+  $('.text-wrapper > [contenteditable="true"]').attr("contenteditable", false);
+  if ($(element).attr("data-editable") == "true") {
+    element.contentEditable = true;
+  }
+
+  element.focus();
 }
